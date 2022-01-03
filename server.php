8d64f79f4cd2c4210ce626a7c2837e7e3852366c
Lodry@ <janusz.filin@langivi.technology>
2022-01-03 18:32:58 +0200

    Inited loader

--- server.php
+++ server.php
@@ -1,17 +1,33 @@
 #!/usr/bin/env php
 <?php
+
+
+use Langivi\ImportantReminder\Loader;
+
+require 'vendor/autoload.php';
+
+echo "hello";
+$loader = Loader::boot();
 $httpServer = new HttpServer(81, "tcp://0.0.0.0");
 $httpServer->setPublicPath(__DIR__ . DIRECTORY_SEPARATOR . "public");
 $result = new finfo();
-$httpServer->on_request(function (HttpRequest $req, HttpResponse $res) use ($result) {
-    if (file_exists($this->publicPath . $req->uri) && !is_dir($this->publicPath . $req->uri)) {
-        $mimeType = $result->file($this->publicPath . $this->uri, FILEINFO_MIME_TYPE);
-        $file = file_get_contents($this->publicPath . $req->uri);
-        echo "Requested URI is $this->publicPath.$req->uri\n";
-        $res->setHeader("Content-Type", $mimeType)
-            ->setHeader("Content-Length", strlen($file));
-        $res->send($file);
 
+function servePublic(string $path, HttpResponse $res, finfo $fileinfo): void
+{
+    $mimeType = $fileinfo->file($path, FILEINFO_MIME_TYPE);
+    $file = file_get_contents($path);
+    echo "Requested URI is $path\n";
+    $res->setHeader("Content-Type", $mimeType)
+        ->setHeader("Content-Length", strlen($file));
+    $res->send($file);
+}
+
+$httpServer->on_request(function (HttpRequest $req, HttpResponse $res) use ($result) {
+    global $loader;
+    var_dump($req);
+    $publicUri = $this->publicPath . $req->uri;
+    if (file_exists($publicUri) && !is_dir($publicUri)) {
+        servePublic($publicUri, $res, $result);
         return;
     }
     $res->setHeader("Content-Type", "text/plain; charset=utf-8");
---
+++ src/Loader.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Langivi\ImportantReminder;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Loader
+{
+    private readonly ContainerBuilder $containerBuilder;
+
+    public function __construct()
+    {
+        $this->containerBuilder = new ContainerBuilder();
+    }
+
+    public function setTemplateEngine()
+    {
+        if (!is_dir(__DIR__ . '/templates')) {
+            mkdir(__DIR__ . '/templates');
+        }
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+
+        $twig = new \Twig\Environment($loader, [
+            'cache' => __DIR__ . '/cache/twig_cache',
+        ]);
+        $this->containerBuilder->set('twig', $twig);
+    }
+
+    public function getContainer()
+    {
+        return $this->containerBuilder;
+    }
+
+    public static function boot()
+    {
+        $object = new self();
+        $object->setTemplateEngine();
+        return $object;
+
+    }
+}
