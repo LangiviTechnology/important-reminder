9d995d490c0ce25bb7d079e917c2a5a955b8ded1
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-10 15:53:14 +0200

    Fix types

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -3,7 +3,7 @@ namespace Langivi\ImportantReminder\Controllers;
 
 class IndexController
 {
-    public function index(HttpRequest $request, HttpResponse $response)
+    public function index(\HttpRequest $request, \HttpResponse $response)
     {
         $response->setHeader("Content-Type", "text/plain; charset=utf-8");
         $response->send("Hello world\n");
