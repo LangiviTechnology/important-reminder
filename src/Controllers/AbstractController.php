8cc21236c673938d561aff838420eeaf70a168c0
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-14 15:32:58 +0200

    Fix abstract controller

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -5,9 +5,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class AbstractController
 {
     private readonly ContainerBuilder $containerBuilder;
-    public function __construct()
-    {
-    }
 
     public function setContainer(ContainerBuilder $container): self
     {
