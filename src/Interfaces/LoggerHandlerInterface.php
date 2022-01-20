892f4ea437db2810906d09bbb8723e03f069e14e
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-20 13:20:43 +0200

    fix log template

--- src/Interfaces/LoggerHandlerInterface.php
+++ src/Interfaces/LoggerHandlerInterface.php
@@ -4,6 +4,6 @@ namespace Langivi\ImportantReminder\Interfaces;
 
 interface LoggerHandlerInterface
 {
-	public const DEFAULT_FORMAT = '%timestamp% [%level%]: %message%';
+	public const DEFAULT_FORMAT = '%timestamp% [%level%]: %message% %context%';
     public function handle(array $vars):void;
 }
