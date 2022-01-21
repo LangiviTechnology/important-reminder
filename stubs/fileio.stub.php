2f6d90241d746c2c95f60214ad4aa05669f0b29b
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-21 16:11:51 +0200

    remove use include path in file func

--- stubs/fileio.stub.php
+++ stubs/fileio.stub.php
@@ -10,8 +10,8 @@ function clear_timeout(int $timerId): bool {}
 function set_interval(callable $callback, int $interval): int {}
 function clear_interval(int $timerId): bool {}
 
-function file_get_contents_async(string $filename, callable $cb, bool $use_include_path = false, int $offset = 0, int $maxlen = null): bool {}
-function file_put_contents_async(string $filename, string $data, callable $cb, bool $use_include_path = false, int $flags = 0): bool {}
+function file_get_contents_async(string $filename, callable $cb, int $offset = 0, int $maxlen = null): bool {}
+function file_put_contents_async(string $filename, string $data, callable $cb, int $flags = 0): bool {}
 function server(int $port, string $host):void{}
 
 class Server{
