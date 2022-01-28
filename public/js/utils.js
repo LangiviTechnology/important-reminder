5fe17bec4e73c3f9251a3184e542b21b34a4c840
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-28 20:25:18 +0200

    add fetch with token

--- public/js/utils.js
+++ public/js/utils.js
@@ -5,3 +5,22 @@ export function checkCorrectEmail(event) {
 	const label = document.querySelector('.email-label');
 	label.textContent = checkedEmail ? '' : 'Write correct email please';
 };
+
+export async function authFetch (resource, originalInit = {}) {
+	const authInit = {...originalInit};
+	if (!authInit.headers) init.headers = {};
+	authInit.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+	const response = await fetch(resource, authInit);
+	if (response.status === 401 && !originalInit._isRetry) {
+		authInit._isRetry = true;
+		const refresh = await fetch('/auth/refresh');
+		if (refresh.status === 401) {
+			return response;
+		}
+		const data = await response.json();
+		localStorage.setItem('token', data.tokens.accessToken);
+		
+		return await authFetch(resource, authInit);
+	}
+	return response;
+}
