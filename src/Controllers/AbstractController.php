ea4e4054671a160224386a6da459b69ec9012a8f
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-28 20:19:59 +0200

    add get cookie method

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -11,4 +11,16 @@ class AbstractController
         $this->containerBuilder = $container;
         return $this;
     }
+
+    public function getCookie(string|null $rawCookie = '', string $name): string|bool
+    {
+        //TODO might need better parser cookie
+        parse_str(strtr($rawCookie, array('&' => '%26', '+' => '%2B', ';' => '&')), $cookies);
+        var_dump('========', $rawCookie, $name);
+        var_dump('========', $cookies);
+        if (!array_key_exists($name, $cookies)){
+            return false; 
+        }
+        return $cookies[$name];
+    }
 }
