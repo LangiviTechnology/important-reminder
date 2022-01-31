b61f5f72e26fd37d21008ed9ba4b87ea2cc596b9
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-01-31 13:32:12 +0200

    rework middle to class

--- src/MiddleWares/AuthMiddleware.php
+++ src/MiddleWares/AuthMiddleware.php
@@ -2,27 +2,42 @@
 
 namespace Langivi\ImportantReminder\MiddleWares;
 
+use Langivi\ImportantReminder\Services\TokenService; 
 
-function getCookie(string|null $rawCookie = '', string $name): string|bool
+class AuthMiddleware
 {
-    //TODO might need better parser cookie
-    parse_str(strtr($rawCookie, array('&' => '%26', '+' => '%2B', ';' => '&')), $cookies);
-    var_dump('========', $rawCookie, $name);
-    var_dump('========', $cookies);
-    if (!array_key_exists($name, $cookies)){
-        return false; 
-    }
-    return $cookies[$name];
-}
+	public function __construct(
+        private TokenService $tokenService,
+	) {
 
-function authMiddleware(\HttpRequest $request, \HttpResponse $response) {
-    $accessToken = getCookie($request->headers['Cookie'], 'accessToken');
-    if (!$accessToken){
-        $response->setHeader("Content-Type", "application/json; charset=utf-8");
-        $response->setStatusCode(401);
-        $response->send(json_encode(
-            (object)['error' => "Unauthorized"]
-        ));
-        return; 
+	}
+
+    public function getCookie(string $rawCookie, string $name): string|bool
+    {
+        //TODO might need better parser cookie
+        parse_str(strtr($rawCookie, array('&' => '%26', '+' => '%2B', ';' => '&')), $cookies);
+        var_dump('========', $rawCookie, $name);
+        var_dump('========', $cookies);
+        if (!array_key_exists($name, $cookies)){
+            return false; 
+        }
+        return $cookies[$name];
+    }
+
+
+    public function middleware(\HttpRequest $request, \HttpResponse $response): bool
+    {
+        $cookie = $request->headers['Cookie'] ?? '';
+        $accessToken = $this->getCookie($cookie, 'accessToken');
+        if (!$accessToken || !$this->tokenService->validationAccessToken($accessToken)) 
+        {
+            $response->setHeader("Content-Type", "application/json; charset=utf-8");
+            $response->setStatusCode(401);
+            $response->send(json_encode(
+                (object)['error' => "Unauthorized"]
+            ));
+            return false;
+        }
+        return true;
     }
 }
