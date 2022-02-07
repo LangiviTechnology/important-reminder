9c73d13feac59c092ea4a7efbd71680f12017d53
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-02-07 18:24:05 +0200

    Fix allow users condition

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -22,7 +22,7 @@ class UserService
         } else {
             $this->allowedUsers = parse_ini_file($allowedUsersFile);
         }
-        if (count($this->allowedUsers)) {
+        if (count($this->allowedUsers) === 0) {
             $this->logger->error('allowed users is empty');
         }
 	}
@@ -90,8 +90,6 @@ class UserService
 
     public function findOne(string $email): User | null
     {
-//         accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiRpZCIsImxvZ2luIjoiQWRtaW4iLCJleHBpcmF0aW9uIjoxNjQzMjE3MTIzfQ.PSAIrHSs4yOx5eFx_G4M9Z1mWIj-iL_N13Um6nPcqU8"
-//        refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IiRpZCIsImxvZ2luIjoiQWRtaW4iLCJleHBpcmF0aW9uIjoxNjQzMjU4NTIzfQ.hG1u_KHAc0I-56wFuRSemeqWNRJ_V786X0tDH8Sz_Kc"
 		$user = new User ('Admin', 'admin@langivi.com', '$2y$10$9sm3gGJqQSvp7Gt0J6s2iOAYmeERwpOXIEZPPLZUE.XPHd3FAK3G.');
         // TODO Get user by mail from repo
 		if ($email == 'admin@langivi.com') return $user;
@@ -100,7 +98,6 @@ class UserService
 
 	public function isAllowed(string $email): bool
     {
-        // var_dump($email, $this->allowedUsers);
         if (array_key_exists($email, $this->allowedUsers)) {
             return true;
         }
