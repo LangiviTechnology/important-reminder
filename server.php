dc6301164604b0fa98816fd52c9302c7a61d14ac
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-02-07 18:13:41 +0200

    Add mixin to response

--- server.php
+++ server.php
@@ -3,8 +3,9 @@
 
 use Langivi\ImportantReminder\Loader;
 use Langivi\ImportantReminder\Routing\HttpMethods;
-// use Langivi\ImportantReminder\Routing\Router;
-use Langivi\ImportantReminder\Services\LoggerService;
+
+// use Langivi\ImportantReminder\Services\LoggerService;
+use Langivi\ImportantReminder\Handlers\ExceptionHandler;
 use function Langivi\ImportantReminder\Utils\getFileMimeType;
 
 require 'vendor/autoload.php';
@@ -20,59 +21,57 @@ echo "Started on PORT " . PORT . PHP_EOL;
 
 function servePublic(string $path, HttpResponse $res, finfo $fileinfo): void
 {
-    // $mimeType = $fileinfo->file($path, FILEINFO_MIME_TYPE);
     $mimeType = getFileMimeType($path);
     $res->setHeader("Content-Type", $mimeType);
-    var_dump($mimeType);
-    // $file = file_get_contents_async($path, fn($arg)=>var_dump($arg)&$res->setHeader("Content-Length", strlen($arg))
-        // ->send($arg)); // FIX PROBLEM WITH ASYNC READ
+    // var_dump($mimeType);
     file_get_contents_async($path, 
         fn(string $arg):HttpResponse|null=>
             $res->setHeader("Content-Length", strlen($arg))->send($arg)); 
         // FIX PROBLEM WITH ASYNC READ
-    echo "Requested URI is $path\n";
+    echo "Requested URI is $path" . PHP_EOL;
 }
 
 $httpServer->on_request(function (HttpRequest $req, HttpResponse $res) use ($result) {
     global $loader;
-    $logger = $loader->getContainer()->get(LoggerService::class);
-    //    file_get_contents_async('server.php', fn($arg)=>var_dump($arg));
-    $publicUri = $this->publicPath . $req->uri;
-    if (file_exists($publicUri) && !is_dir($publicUri)) {
-        servePublic($publicUri, $res, $result);
-        return;
+    $res->_type = 'html';
+    if ($req->headers["Sec-Fetch-Mode"] === 'cors') {
+        $res->_type = 'json';
     }
-    /**
-     * @var $router Router
-     */
-    // var_dump($req);
-    $router = $loader->getContainer()->get('router');
-    $method = HttpMethods::tryFrom($req->method);
+    // $logger = $loader->getContainer()->get(LoggerService::class);
+    
+    //    file_get_contents_async('server.php', fn($arg)=>var_dump($arg));
+    try {
+        $publicUri = $this->publicPath . $req->uri;
+        if (file_exists($publicUri) && !is_dir($publicUri)) {
+            servePublic($publicUri, $res, $result);
+            return;
+        }
+        /**
+         * @var $router Router
+         */    
+        $router = $loader->getContainer()->get('router');
+        $method = HttpMethods::tryFrom($req->method);
 
-    // TODO: Add Main error handler
-    if (!$method) {
-        $res->setStatusCode(404);
-        $res->send('Incorrect method' . $req->method);
-        $logger->error('Incorrect method ' . $req->method);
-        return;
+        // TODO: Add Main error handler
+        if (!$method) {
+            throw new Exception('Incorrect method' . $req->method, 404);
+        }
+        
+        $route = $router->matchFromPath($req->uri, $method);
+        if (!$route) {
+            throw new Exception('Path not found ' . $req->uri, 404);
+        }
+        $route->call($req, $res);
+
+    } catch (\Throwable $th) {
+        $exceptionHandler = $loader->getContainer()->get(ExceptionHandler::class);
+        $exceptionHandler->sendError($res, $th->getMessage(), $th->getCode(), ['uri' => $req->uri, 'method' => $method->value]);
     }
     
-    $route = $router->matchFromPath($req->uri, $method);
-    if (!$route) {
-        $res->setStatusCode(404);
-        $res->send('Path not found ' . $req->uri);
-        $logger->warning('Path not found: ', ['uri' => $req->uri, 'method' => $method->value]);
-        return;
-    }
-    // throw new Error('test error');
-    $route->call($req, $res);
-
 });
 
 $httpServer->on_error(function ($error){
-    // global $loader;
-    // $logger = $loader->getContainer()->get(LoggerService::class);
-    // echo 'error=================';
+    echo 'error=================';
     var_dump($error);
     // $logger->error('', $error);
 });
