ac5a182019b08cd2ce7b809a16e84a3f04a76e0d
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-02-08 19:56:16 +0200

    Fix remove twig to handler

--- src/Loader.php
+++ src/Loader.php
@@ -8,7 +8,6 @@ use Symfony\Component\DependencyInjection\{ContainerBuilder, Loader\PhpFileLoade
 use Langivi\ImportantReminder\Routing\Router;
 use Langivi\ImportantReminder\Services\LoggerService;
 use Langivi\ImportantReminder\Services\TokenService;
-use Langivi\ImportantReminder\Handlers\ExceptionHandler;
 
 
 class Loader
@@ -105,15 +104,6 @@ class Loader
         return $this;
     }
 
-    public function setupErrorHandler()
-    {
-        echo 'Setup Error handler' . PHP_EOL;
-        $twig = $this->containerBuilder->get('twig');
-        $errorHandler = $this->containerBuilder->get(ExceptionHandler::class);
-        $errorHandler->setTwig($twig);
-        return $this;
-    }
-
     public function getContainer()
     {
         return $this->containerBuilder;
@@ -130,7 +120,6 @@ class Loader
         $object->containerBuilder->compile();
         $object->setupLogger();
         $object->setupTokenService();
-        $object->setupErrorHandler();
         $object->containerBuilder->get(LoggerService::class)->info('Server started');
         // var_dump($object->containerBuilder->get(IndexController::class));
 //        var_dump($object->containerBuilder->getParameter('env'));
