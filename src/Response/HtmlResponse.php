bb60e3a644b7c859e1ef3e269b8779b8f098a456
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-02-08 19:57:30 +0200

    Rework html response

--- src/Response/HtmlResponse.php
+++ src/Response/HtmlResponse.php
@@ -2,23 +2,32 @@
 
 namespace Langivi\ImportantReminder\Response;
 
+use Error;
 use Langivi\ImportantReminder\Loader;
 
 class HtmlResponse extends AbstractResponse
 {
+    private string $template;
 
-    public function prepare(string $value)
+    public function setTemplate(string $template)
     {
-        if (null !== ($container = Loader::get()?->getContainer())) {
-            $twig = $container->get('twig');
-        }
-
-        $this->response->setHeader("Content-Type", "text/html; charset=utf-8");
-        return $twig->render($value);
+        $this->template = $template;
     }
 
-    public function error(array $value)
+    public function prepare(array|object $payload)
     {
-        $this->send(.....);
+        if (null === ($container = Loader::get()?->getContainer())) {
+            $this->response->setStatusCode(500);
+            return 'Error template engine not found';
+        }
+        $twig = $container->get('twig');
+        $this->response->setHeader("Content-Type", "text/html; charset=utf-8");
+        return $twig->render($this->template, $payload);
+    }
+
+    public function error(array|object $payload)
+    {
+        $this->template = 'error.twig';
+        $this->send(['title' => 'Error', ...$payload]);
     }
 }
