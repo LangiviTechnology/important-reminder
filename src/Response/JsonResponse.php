ab12a1dd11868a97305f49220a00ab306d80ce49
MT <86599029+andriy-kozlytski@users.noreply.github.com>
2022-02-08 19:57:11 +0200

    Rework json response

--- src/Response/JsonResponse.php
+++ src/Response/JsonResponse.php
@@ -3,10 +3,13 @@ namespace Langivi\ImportantReminder\Response;
 
 class JsonResponse extends AbstractResponse
 {
-    public function prepare(string $value)
+    public function prepare(array|object $value)
     {
         $this->response->setHeader("Content-Type", "application/json");
         return json_encode($value);
     }
-
+    public function error(array|object $value)
+    {
+        $this->send($value);
+    }
 }
